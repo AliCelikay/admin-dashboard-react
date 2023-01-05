4c3e02a756ff3158a87beffc0c646d7ca09dd7da
AliCelikay <alicelikay129@gmail.com>
2023-01-05 13:10:33 -0600

    added categories to side bar

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
+import { ContextProvider} from './contexts/ContextProvider';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// inside of app now we can use all the providers, but must import first
+ReactDOM.render(
+    <ContextProvider>
+        <App />
+    </ContextProvider>,
+    document.getElementById('root')
+);
