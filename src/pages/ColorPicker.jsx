82d2c019c0d7b928fa3b0ed90c7e6b2a9c29862f
AliCelikay <alicelikay129@gmail.com>
2023-01-11 08:43:31 -0600

    updated color page

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -3,6 +3,11 @@ import { ColorPickerComponent } from '@syncfusion/ej2-react-inputs';
 
 import { Header } from '../components'
 
+// function to change the color of the pen on screen of page
+const change = (args) => {
+  document.getElementById('preview').style.backgrounColor = args.currentValue.hex;
+}
+
 const ColorPicker = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
@@ -18,6 +23,8 @@ const ColorPicker = () => {
               modeSwitcher={false}
               inline
               showButtons={false}
+              change={change}
+              
             />
           </div>
           <div>
@@ -28,6 +35,7 @@ const ColorPicker = () => {
               modeSwitcher={false}
               inline
               showButtons={false}
+              change={change}
             />
           </div>
         </div>
